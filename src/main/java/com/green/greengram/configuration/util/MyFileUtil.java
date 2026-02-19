dd8ba4ac0278680d3fb76198922b41f0818067be
sbsteacher <id@gmail.com>
2026-02-19 11:17:47 +0900

    first

---
+++ src/main/java/com/green/greengram/configuration/util/MyFileUtil.java
@@ -0,0 +1,13 @@
+package com.green.greengram.configuration.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j //로그
+@Component //빈등록
+public class MyFileUtil {
+    public final String fileUploadPath;
+
+    //String값 하나 파라미터로 받는 생성자 만든다
+
+}
