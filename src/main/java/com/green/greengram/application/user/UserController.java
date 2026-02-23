7f044ea6bd834a4aac5c5f92386a3f5cf6b0746d
sbsteacher <id@gmail.com>
2026-02-23 16:51:51 +0900

    프로파일 이미지 수정 작업 완료

--- src/main/java/com/green/greengram/application/user/UserController.java
+++ src/main/java/com/green/greengram/application/user/UserController.java
@@ -65,7 +65,7 @@ public class UserController {
     public ResultResponse<?> patchProfileUserPic(@AuthenticationPrincipal UserPrincipal userPrincipal
                                                , @RequestPart MultipartFile pic) {
         String savedFileName = userService.patchProfilePic(userPrincipal.getSignedUserId(), pic);
-        return null;
+        return new ResultResponse<>("프로파일 유저 사진 수정", savedFileName);
     }
 
 }
