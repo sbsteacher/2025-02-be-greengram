b9bb2a35c03e5832a1dd0648d81c04d9736fa17b
sbsteacher <id@gmail.com>
2026-02-24 10:45:27 +0900

    feed_like 작업 중

---
+++ src/main/java/com/green/greengram/application/feedlike/FeedLikeController.java
@@ -0,0 +1,28 @@
+package com.green.greengram.application.feedlike;
+
+import com.green.greengram.application.feedlike.model.FeedLikeReq;
+import com.green.greengram.configuration.model.ResultResponse;
+import com.green.greengram.configuration.model.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/feed/like")
+public class FeedLikeController {
+    private final FeedLikeService feedLikeService;
+
+    @PostMapping
+    public ResultResponse<?> toggleFeedLike(@AuthenticationPrincipal UserPrincipal userPrincipal
+                                          , @RequestBody FeedLikeReq req) {
+        req.setSignedUserId( userPrincipal.getSignedUserId() );
+        log.info("req: {}", req);
+        return null;
+    }
+}
---
+++ src/main/java/com/green/greengram/application/feedlike/FeedLikeMapper.java
@@ -0,0 +1,7 @@
+package com.green.greengram.application.feedlike;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FeedLikeMapper {
+}
