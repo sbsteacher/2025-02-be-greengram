503ab51f787b38c281e4c5260a823ece9ac830ea
sbsteacher <id@gmail.com>
2026-02-26 12:12:46 +0900

    피드 삭제 완료

--- src/main/java/com/green/greengram/application/feed/FeedMapper.java
+++ src/main/java/com/green/greengram/application/feed/FeedMapper.java
@@ -1,5 +1,6 @@
 package com.green.greengram.application.feed;
 
+import com.green.greengram.application.feed.model.FeedDeleteReq;
 import com.green.greengram.application.feed.model.FeedGetReq;
 import com.green.greengram.application.feed.model.FeedGetRes;
 import com.green.greengram.application.feed.model.FeedPostReq;
@@ -12,7 +13,8 @@ import java.util.List;
 public interface FeedMapper {
     int save(FeedPostReq req);
     List<FeedGetRes> findAll(FeedGetReq req);
-
+    int delete(FeedDeleteReq req);
+    int deleteRef(FeedDeleteReq req);
 
     int savePics(@Param("feedId") long feedId
                , @Param("picSavedNames") List<String> picSavedNames);
