9614e6b8e1413b14567de37c85b9f29684ccd540
sbsteacher <id@gmail.com>
2026-03-20 17:04:34 +0900

    배포 후 새로고침 화면 표시되게 처리

--- src/main/java/com/green/greengram/configuration/WebMvcConfiguration.java
+++ src/main/java/com/green/greengram/configuration/WebMvcConfiguration.java
@@ -1,10 +1,13 @@
 package com.green.greengram.configuration;
 
-
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import java.io.IOException;
 
 @Configuration //빈등록
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -18,6 +21,22 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pic/**")
                 .addResourceLocations("file:" + fileUploadPath);
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource resource = location.createRelative(resourcePath);
+
+                        if(resource.exists() && resource.isReadable()) {
+                            return resource;
+                        }
+
+                        return new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 
 }
